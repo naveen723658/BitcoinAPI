621b393f5b6a5ff89b277a11b6f50498dd00a000
Naveen Kumar <106751177+naveen723658@users.noreply.github.com>
2022-06-03 01:00:39 +0530

    Update settings.py

--- carefi/settings.py
+++ carefi/settings.py
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.environ.get('SECRET_KEY', default='your secret key')'
+SECRET_KEY = 'django-insecure-*dpnt&cds_3-5zunb(&a_vkowqz$v)z7_+0@u^ndadj4bhl-&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'True'
 
 
 
 ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
